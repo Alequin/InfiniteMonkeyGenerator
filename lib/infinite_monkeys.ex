0de3ee6e01f1174982a7158e625daf0b2a4b27d5
James Cox <j-cox03@hotmail.com>
2017-12-03 19:03:49 +0000

    Made code to generate random text of length given length

--- lib/infinite_monkeys.ex
+++ lib/infinite_monkeys.ex
@@ -4,15 +4,33 @@ defmodule InfiniteMonkeys do
     IO.puts "Infinite Monkeys: running"
 
     case File.read("./english-words/words_alpha.txt") do
-      {:ok, content} -> begin_search(content)
+      {:ok, content} -> begin_search(String.split(content, "\n"))
       {:error, _reason} -> "File does not exist"
     end
 
     IO.puts "Infinite Monkeys: exit"
   end
 
-  defp begin_search(words) do
-    IO.puts words
+  def begin_search(words) do
+    text = generate_text(10000)
+
+    for(word <- words) do
+      IO.puts word
+    end
+
+    IO.puts text
+  end
+
+  def generate_text(charCount) do
+    letter = InfiniteMonkeys.generate_random_letter
+    if(charCount > 0) do
+      "#{letter}#{InfiniteMonkeys.generate_text(charCount-1)}"
+    end
+  end
+
+  def generate_random_letter do
+    letter_code = 96 + :rand.uniform(26)
+    <<letter_code :: utf8>>
   end
 
 end
--- test/infinite_monkeys_test.exs
+++ test/infinite_monkeys_test.exs
@@ -2,7 +2,21 @@ defmodule InfiniteMonkeysTest do
   use ExUnit.Case
   doctest InfiniteMonkeys
 
-  test "greets the world" do
-    assert 1 == 1
+  test "can generate random text - length 5" do
+    expected = 5
+    result = String.length(InfiniteMonkeys.generate_text(5))
+    assert expected == result
+  end
+
+  test "can generate random text - length 10" do
+    expected = 10
+    result = String.length(InfiniteMonkeys.generate_text(10))
+    assert expected == result
+  end
+
+  test "can generate random text - length 10000" do
+    expected = 10000
+    result = String.length(InfiniteMonkeys.generate_text(10000))
+    assert expected == result
   end
 end
